d27835ec3b3b602cb9175fc816464fce1799333e
Devix Rootix <devixrootix@gmail.com>
2020-04-04 22:26:10 +0500

    Fixed typo in settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/'
 
